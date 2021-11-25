0d93fabecd655d5f1e3f81198ab6d207f5eb6ca2
Mohamed Magdy Mohamed <MohamedMM@hits-egypt.local>
2021-11-25 16:50:17 +0200

    first commit

---
+++ Chating/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chating.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SentMessage(string fromuser,string message)
+        {
+            await Clients.All.SendAsync("recieveMessage", fromuser, message);
+        }
+    }
+}
---
+++ Chating/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+connection.on("recieveMessage", function (fromuser, message) {
+    var Message = `${fromuser} : ${message}`;
+    var item = document.createElement('li');
+    item.textContent = Message;
+    $('#chat-list').prepend(item);
+});
+connection.start();
+$('#btnSend').on("click", function () {
+    var fromuser= $('#user').val();
+    var message = $('#message').val();
+    connection.invoke("SentMessage", fromuser, message);
+    $('#user').val("");
+    $('#message').val("");
+});
